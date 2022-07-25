43df60ededf2ec65b6cad55029e3988d87c27107
Nelson Correia <nelson.ncdesigns@gmail.com>
2022-07-25 04:15:31 +0000

    Views directory

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 const express=require('express');
 const bodyParser=require('body-parser');
 const exphbs=require('express-handlebars');
+const path=require('path');
 const nodemailer=require('nodemailer');
 
 const app=express();
 
+// View engine setup
+app.engine('handlebars', exphbs.engine({
+    extname:'handlebars',
+    defaultLayout: 'contact',
+    layoutsDir:'views'
+}));
+app.set('view engine', 'handlebars');
+
+// Static folder
+app.use('/public',express.static(path.join(__dirname,'public')));
+
+// Body Parser Middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 app.get('/',(req,res)=>{
-    res.send('Hello');
+    res.render('contact');
 });
 
-app.listen(3000,()=>{
-    console.log('Server started.');
+app.listen(8080,()=>{
+    console.log('Server started...');
 });
